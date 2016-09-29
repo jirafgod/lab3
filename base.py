096ca15cab11e4fe4f51e7512ce4c97284b5848c
jirafgod <kikonshin@gmail.com>
2016-09-29 17:07:39 +0300

    Добавил класс vk_api

--- base.py
+++ base.py
@@ -1,3 +1,8 @@
+import requests
+import json
+import time
+from collections import Counter
+
 class BaseClient:
     BASE_URL = None
 
@@ -31,3 +36,52 @@ class BaseClient:
             self.method,
             http_method=self.http_method
         )
+    
+class vk_api(BaseClient):
+    user_id,params,username = None,'',None 
+    age = []
+    
+    def __init__(self, username):
+        self.BASE_URL = "http://api.vk.com/method/"
+        self.http_method = "GET"
+        self.username = username
+        
+    def get_data(self,method,params):
+        self.method = method
+        r = requests.get(self.generate_url(self.method),params)
+        data = r.json()
+        return data
+    
+    def get_params(self, **kwargs):
+        for key in kwargs:
+            self.params = self.params+"&"+key+"="+kwargs[key]
+        return self.params
+    
+    def get_id(self):
+        self.user_id = self.get_data("users.get",self.get_params(uids=self.username,v='3.0'))["response"][0]["uid"]
+        return self.user_id
+    
+    def get_friends(self):
+        friends = self.get_data("friends.get",self.get_params(user_id=str(self.user_id),fields="bdate",v='3.0'))["response"]
+        for friend in friends:
+            if friend.get("bdate"):
+                date = friend["bdate"].split(".")
+                if len(date)>2:
+                    self.age.append(int((int(time.time())-int(time.mktime(time.strptime(friend["bdate"], '%d.%m.%Y'))))/31536000))
+        self.age = dict(Counter(self.age))
+        return self.age
+    
+    def print_sharp(self,count):
+        sharp = '';
+        for i in range(count):
+            sharp = sharp + "#"
+        return sharp
+             
+    def print_age(self):
+        for i in self.age:
+            print("Age {} count {}".format(i,self.print_sharp(self.age[i])))
+    
+    def exe(self):
+        self.get_id()
+        self.get_friends()
+        self.print_age()
